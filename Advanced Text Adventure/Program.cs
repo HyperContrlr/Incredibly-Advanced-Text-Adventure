c90c6de333240f4116168171648e123e64b82959
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2024-10-28 13:25:59 -0700

    greatest of all time

--- Advanced Text Adventure/Program.cs
+++ Advanced Text Adventure/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Advanced_Text_Adventure
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Advanced_Text_Adventure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter your name");
+            string playerName = (Console.ReadLine());
+            Console.WriteLine("Your name is " + playerName + "?");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Alrighty then. I'm Jermy, creator of these little guys,")
         }
     }
 }
