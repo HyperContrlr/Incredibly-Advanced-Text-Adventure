7e4db6f7ad65bd68834491ffa40f8204d47ed08a
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2024-10-30 12:58:55 -0700

    Created the petBit class, introduced some dialogue for the start

--- Advanced Text Adventure/Program.cs
+++ Advanced Text Adventure/Program.cs
@@ -1,17 +1,73 @@
 ﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Advanced_Text_Adventure
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your name");
-            string playerName = (Console.ReadLine());
-            Console.WriteLine("Your name is " + playerName + "?");
+            string playerName = Console.ReadLine();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           //hehehaha
+            if (playerName == "IRS")
+            {
+                Console.WriteLine("You're not supposed to be here");
+                System.Threading.Thread.Sleep(3000);
+                Environment.Exit(0);
+            }
+            else if (playerName == "irs")
+            {
+                Console.WriteLine("You're not supposed to be here");
+                System.Threading.Thread.Sleep(3000);
+                Environment.Exit(0);
+            }
+            else if (playerName == "Jermy")
+            {
+                 Console.WriteLine("Well, that'll be confusing.");
+            }
+            else
+            {
+                Console.WriteLine("Your name is " + playerName + "?");
+            }
             System.Threading.Thread.Sleep(1000);
-            Console.WriteLine("Alrighty then. I'm Jermy, creator of these little guys,")
+            Console.WriteLine("Alrighty then, " + playerName + ". I'm Jermy, creator of these little guys, and I'm entrusting one to you." );
+            System.Threading.Thread.Sleep(5000);
+
+            while (true)
+            {
+                Console.WriteLine("Take your pick:");
+                Console.WriteLine(bitOne.name + ", " + bitTwo.name + ", " + bitThree.name);
+                string SelectedBit = Console.ReadLine();
+                if (("Byte") == SelectedBit)
+                {
+                    Console.WriteLine("Ah Byte, the boy. You know, Byte wasn't the first bit ever made,");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.WriteLine("But he was the lone survivior of the Purge Protocol");
+                    System.Threading.Thread.Sleep(2000);
+                    break;
+                }
+                else if ("Michael" == SelectedBit)
+                {
+                    Console.WriteLine("Placeholder");
+                    break;
+                }
+                else if ("BitByte" == SelectedBit)
+                {
+                    Console.WriteLine("Placeholder");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("... did you misspell something?");
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+            Console.WriteLine("Good Choice!");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("But now, you're on your own. ");
+
         }
+        
     }
 }
---
+++ Advanced Text Adventure/petBit.cs
@@ -0,0 +1,32 @@
+﻿namespace Advanced_Text_Adventure
+{
+    internal partial class Program
+    {
+        public static petBit bitOne = new petBit("Byte", 10, 1, 1, 10, 5, 1);
+        public static petBit bitTwo = new petBit("Michael", 3, 1, 1, 5, 1, 1);
+        public static petBit bitThree = new petBit("BitByte", 5, 1, 1, 20, 10, 1);
+        public class petBit
+        {
+            public string name;
+
+            public int neededLight;
+            public int minimumLight;
+            public int possessedLight;
+
+            public float healthiness;
+            public float happiness;
+            public float loyalty;
+
+            public petBit(string name, int neededLight, int minimumLight, int possessedLight, float healthiness, float happiness, float loyalty)
+            {
+                this.name = name;
+                this.neededLight = neededLight;
+                this.minimumLight = minimumLight;
+                this.possessedLight = possessedLight;
+                this.healthiness = healthiness;
+                this.happiness = happiness;
+                this.loyalty = loyalty;
+            }
+        }
+    }
+}
